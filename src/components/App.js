c0ba5b39ca53cf5012aa89f5e58bc150d6628f09
Nikhil <nkarnwal07@gmail.com>
2022-05-14 15:59:44 +0530

    Initial theme added with an empty object

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import Header from "./ui/Header";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from './ui/Theme'
 
 const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header />
-      {[...new Array(1200)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join("\n")}
-    </div>
+    </ThemeProvider>
   );
 };
 
