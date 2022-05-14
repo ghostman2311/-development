3997876d117347e6de341172f29e97207aad0ac1
Nikhil <nkarnwal07@gmail.com>
2022-05-14 15:17:56 +0530

    App bar added with elevation scroll

--- src/components/App.js
+++ src/components/App.js
@@ -1,137 +1,20 @@
-import React, { useState } from "react";
-import { ThemeProvider } from "@material-ui/styles";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import theme from "./ui/Theme";
+import React from "react";
 import Header from "./ui/Header";
-import Footer from "./ui/Footer";
-import LandingPage from "./LandingPage";
-import Services from "./Services";
-import CustomSoftware from "./CustomSoftware";
-import MobileApps from "./MobileApps";
-import Websites from "./Websites";
-import Revolution from "./Revolution";
-import About from "./About";
-import Contact from "./Contact";
-import Estimate from "./Estimate";
 
-function App() {
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [value, setValue] = useState(0);
-
+const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header
-          value={value}
-          setValue={setValue}
-          selectedIndex={selectedIndex}
-          setSelectedIndex={setSelectedIndex}
-        />
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={props => (
-              <LandingPage
-                {...props}
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/services"
-            render={props => (
-              <Services
-                {...props}
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/customsoftware"
-            render={props => (
-              <CustomSoftware
-                {...props}
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/mobileapps"
-            render={props => (
-              <MobileApps
-                {...props}
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/websites"
-            render={props => (
-              <Websites
-                {...props}
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/revolution"
-            render={props => (
-              <Revolution
-                {...props}
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/about"
-            render={props => (
-              <About
-                {...props}
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/contact"
-            render={props => (
-              <Contact
-                {...props}
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/estimate"
-            render={props => (
-              <Estimate
-                {...props}
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            )}
-          />
-        </Switch>
-        <Footer setValue={setValue} setSelectedIndex={setSelectedIndex} />
-      </BrowserRouter>
-    </ThemeProvider>
+    <div>
+      <Header />
+      {[...new Array(1200)]
+        .map(
+          () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+        )
+        .join("\n")}
+    </div>
   );
-}
+};
 
 export default App;
