a873ddea575a282cc7e922503cba14402949bb12
Nikhil <nkarnwal07@gmail.com>
2022-05-21 18:10:05 +0530

    active tab refresh fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AppBar, Toolbar, Tabs, Tab, Button } from "@material-ui/core";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/styles";
@@ -43,11 +43,38 @@ const useStyle = makeStyles((theme) => ({
     marginRight: "25px",
     height: "45px",
   },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
 }));
 
 const Header = (props) => {
   const classes = useStyle();
   const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    }
+    if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    }
+    if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    }
+    if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    }
+    if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    }
+    if (window.location.pathname === "/estimate") {
+      setValue(5);
+    }
+  }, [value]);
 
   const handleChange = (e, value) => {
     setValue(value);
@@ -57,7 +84,15 @@ const Header = (props) => {
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <img className={classes.logo} src={logo} alt="Company logo" />
+            <Button
+              component={Link}
+              to="/"
+              disableRipple
+              className={classes.logoContainer}
+              onChange={() => setValue(0)}
+            >
+              <img className={classes.logo} src={logo} alt="Company logo" />
+            </Button>
             <Tabs
               value={value}
               indicatorColor="primary"
