13512eda2fb5c537965dc174ba4b110947d31426
Nikhil <nkarnwal07@gmail.com>
2022-06-18 17:21:36 +0530

    creating the hero block

--- src/components/LandigPage.js
+++ src/components/LandigPage.js
@@ -2,6 +2,9 @@ import React from "react";
 import Lottie from "react-lottie";
 import { makeStyles } from "@material-ui/styles";
 import animationData from "../animations/landinganimation/data";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ButtonArrow from "./ButtonArrow";
 
 const useStyle = makeStyles((theme) => ({}));
 
@@ -16,7 +19,32 @@ const defaultOptions = {
 
 const LandingPage = () => {
   const classes = useStyle();
-  return <Lottie options={defaultOptions} height={"100%"} width={"100%"} />;
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Grid container direction="row">
+          <Grid item>
+            <div>
+              Bringing the Indian Technlogy <br /> to the west
+            </div>
+            <Grid container direction="row">
+              <Grid item>
+                <Button variant="contained">Free Estimate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined">
+                  Learn More <ButtonArrow width={15} height={15} fill="red" />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Lottie options={defaultOptions} width={"100%"} height={"100%"} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LandingPage;
